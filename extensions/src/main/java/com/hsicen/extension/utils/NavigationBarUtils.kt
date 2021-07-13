d296d9d54d8532a3811db7ef60bf549a9fd448ee
hsicen <codinghuang@163.com>
2021-07-13 18:27:39 +0800

    底部导航栏监听

--- extensions/src/main/java/com/hsicen/extension/utils/NavigationBarUtils.kt
+++ extensions/src/main/java/com/hsicen/extension/utils/NavigationBarUtils.kt
@@ -17,102 +17,128 @@ import androidx.appcompat.widget.FitWindowsLinearLayout
  * 邮箱：codinghuang@163.com
  * 功能：
  * 描述：虚拟导航栏工具类
+ * 使用：最好在基类Activity中添加全局回调方法
+ * ```
+ *  private var mContent: FrameLayout? = null
+ *  private var mLayoutComplete = false
+ *  private var mNavigationBar = false
+ *
+ *  onCreate(){
+ *      mContent = findViewById(android.R.id.content)
+ *      mContent?.post {
+ *          mNavigationBar = NavigationBarUtils.navigationBarIsOpen(this)
+ *          mLayoutComplete = true
+ *      }
+ *      mContent?.viewTreeObserver?.addOnGlobalLayoutListener(this)
+ *  }
+ *
+ *  override fun onGlobalLayout() {
+ *      if (!mLayoutComplete) return
+ *      if (mNavigationBar != NavigationBarUtils.navigationBarIsOpen(this)) {
+ *          mNavigationBar = NavigationBarUtils.navigationBarIsOpen(this)
+ *          onNavigationBarChange() //方法回调
+ *      }
+ * }
+ *
+ * onDestroy(){
+ *     mContent?.viewTreeObserver?.removeOnGlobalLayoutListener(this)
+ * }
  */
 object NavigationBarUtils {
 
-  /*** false 关闭了NavigationBar ,true 开启了*/
-  fun navigationBarIsOpen(context: Context): Boolean {
-    val rootLinearLayout = findRootLinearLayout(context)
-    var navigationBarHeight = 0
-    if (rootLinearLayout != null) {
-      val layoutParams = rootLinearLayout.layoutParams as MarginLayoutParams
-      navigationBarHeight = layoutParams.bottomMargin
-    }
-
-    return navigationBarHeight != 0
-  }
-
-
-  /*** 导航栏高度，关闭的时候返回0,开启时返回对应值*/
-  fun getNavigationBarHeight(context: Context): Int {
-    val rootLinearLayout = findRootLinearLayout(context)
-    var navigationBarHeight = 0
-    if (rootLinearLayout != null) {
-      val layoutParams = rootLinearLayout.layoutParams as MarginLayoutParams
-      navigationBarHeight = layoutParams.bottomMargin
-    }
-    return navigationBarHeight
-  }
-
-  /**
-   * 从R.id.content从上遍历，拿到 DecorView 下的唯一子布局LinearLayout
-   * 获取对应的bottomMargin 即可得到对应导航栏的高度，0为关闭了或没有导航栏
-   */
-  private fun findRootLinearLayout(context: Context): ViewGroup? {
-    var onlyLinearLayout: ViewGroup? = null
-    try {
-      val window = getWindow(context)
-      window?.let {
-        val decorView = it.decorView as ViewGroup
-        val activity = getActivity(context)
-        val contentView = activity?.findViewById<View>(android.R.id.content)
-        contentView?.let { cv ->
-          var tempView = cv
-          while (tempView.parent !== decorView) {
-            val parent = tempView.parent as ViewGroup
-            if (parent is LinearLayout) {
-              if (parent is FitWindowsLinearLayout) {
-                tempView = parent
-                continue
-              } else {
-                onlyLinearLayout = parent
-                break
-              }
-            } else {
-              tempView = parent
-            }
-          }
+    /*** false 关闭了NavigationBar ,true 开启了*/
+    fun navigationBarIsOpen(context: Context): Boolean {
+        val rootLinearLayout = findRootLinearLayout(context)
+        var navigationBarHeight = 0
+        if (rootLinearLayout != null) {
+            val layoutParams = rootLinearLayout.layoutParams as MarginLayoutParams
+            navigationBarHeight = layoutParams.bottomMargin
         }
-      }
-    } catch (e: Exception) {
-      e.printStackTrace()
-    }
-    return onlyLinearLayout
-  }
 
-  private fun getWindow(context: Context): Window? {
-    return if (getAppCompActivity(context) != null) {
-      getAppCompActivity(context)?.window
-    } else {
-      scanForActivity(context)?.window
+        return navigationBarHeight != 0
     }
-  }
 
-  private fun getActivity(context: Context): Activity? {
-    return if (getAppCompActivity(context) != null) {
-      getAppCompActivity(context)
-    } else {
-      scanForActivity(context)
-    }
-  }
 
-  private fun getAppCompActivity(context: Context?): AppCompatActivity? {
-    if (context == null) return null
-    if (context is AppCompatActivity) {
-      return context
-    } else if (context is ContextThemeWrapper) {
-      return getAppCompActivity(context.baseContext)
+    /*** 导航栏高度，关闭的时候返回0,开启时返回对应值*/
+    fun getNavigationBarHeight(context: Context): Int {
+        val rootLinearLayout = findRootLinearLayout(context)
+        var navigationBarHeight = 0
+        if (rootLinearLayout != null) {
+            val layoutParams = rootLinearLayout.layoutParams as MarginLayoutParams
+            navigationBarHeight = layoutParams.bottomMargin
+        }
+        return navigationBarHeight
     }
-    return null
-  }
 
-  private fun scanForActivity(context: Context?): Activity? {
-    if (context == null) return null
-    if (context is Activity) {
-      return context
-    } else if (context is ContextWrapper) {
-      return scanForActivity(context.baseContext)
+    /**
+     * 从R.id.content从上遍历，拿到 DecorView 下的唯一子布局LinearLayout
+     * 获取对应的bottomMargin 即可得到对应导航栏的高度，0为关闭了或没有导航栏
+     */
+    private fun findRootLinearLayout(context: Context): ViewGroup? {
+        var onlyLinearLayout: ViewGroup? = null
+        try {
+            val window = getWindow(context)
+            window?.let {
+                val decorView = it.decorView as ViewGroup
+                val activity = getActivity(context)
+                val contentView = activity?.findViewById<View>(android.R.id.content)
+                contentView?.let { cv ->
+                    var tempView = cv
+                    while (tempView.parent !== decorView) {
+                        val parent = tempView.parent as ViewGroup
+                        if (parent is LinearLayout) {
+                            if (parent is FitWindowsLinearLayout) {
+                                tempView = parent
+                                continue
+                            } else {
+                                onlyLinearLayout = parent
+                                break
+                            }
+                        } else {
+                            tempView = parent
+                        }
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return onlyLinearLayout
     }
-    return null
-  }
+
+    private fun getWindow(context: Context): Window? {
+        return if (getAppCompActivity(context) != null) {
+            getAppCompActivity(context)?.window
+        } else {
+            scanForActivity(context)?.window
+        }
+    }
+
+    private fun getActivity(context: Context): Activity? {
+        return if (getAppCompActivity(context) != null) {
+            getAppCompActivity(context)
+        } else {
+            scanForActivity(context)
+        }
+    }
+
+    private fun getAppCompActivity(context: Context?): AppCompatActivity? {
+        if (context == null) return null
+        if (context is AppCompatActivity) {
+            return context
+        } else if (context is ContextThemeWrapper) {
+            return getAppCompActivity(context.baseContext)
+        }
+        return null
+    }
+
+    private fun scanForActivity(context: Context?): Activity? {
+        if (context == null) return null
+        if (context is Activity) {
+            return context
+        } else if (context is ContextWrapper) {
+            return scanForActivity(context.baseContext)
+        }
+        return null
+    }
 }
